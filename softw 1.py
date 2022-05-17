48917d9c038102a2ba6a6d921f8eebcdd8bf08ec
TheNemexys <105744319+TheNemexys@users.noreply.github.com>
2022-05-17 20:13:10 +0200

    Add files via upload

---
+++ softw 1.py
@@ -0,0 +1,219 @@
+tentativo1 = 5
+
+print("\nSe nella strada vorrai avanzare questi indovinelli dovrai districare")
+print("\nHai 5 tentativi o come pegno del tuo fallimento la tua vita mi dovrai donare")
+print("\nAccetti la sfida?")
+
+risposta = input()
+
+
+if risposta == "si" or risposta == "va bene" or risposta == "ok":
+    print("\nPossiamo cominciare..")
+
+else:
+    print("\nTorna quando ti saranno cresciute le palle..")
+
+if risposta == "si" or risposta == "va bene" or risposta == "ok":
+    while tentativo1 >= 1:
+        print("\nRadici invisibili ha, più in alto degli alberi sta, lassù tra le nuvole va e mai tuttavia crescerà.")
+        risposta2 = input()
+        if risposta2 == "la montagna" or risposta2 == "montagna":
+            print("\nRisposta esatta!")
+            break
+
+        else:
+            tentativo1 -= 1
+            if tentativo1 == 0:
+                print("SEI MORTOOOOO!!!!!")
+
+            if tentativo1 == 1:
+                print("\nè il tuo ultimo tentativo.. fanne buon uso..")
+
+            if tentativo1 > 1:
+                print("\nhai " + str(tentativo1) + " tentativi prima della tua morte..")
+
+if risposta == "si" or risposta == "va bene" or risposta == "ok":
+    while tentativo1 >= 1:
+        print("\nNon ha voce e grida fa, non ha ali e volo fa, non ha denti e morsi dà, non ha bocca e versi fa.")
+        risposta3 = input()
+        if risposta3 == "il vento" or risposta3 == "vento":
+            print("\nRisposta esatta!")
+            break
+        else:
+            tentativo1 -= 1
+            if tentativo1 == 0:
+                print("SEI MORTOOOOO!!!!!")
+                break
+
+            if tentativo1 == 1:
+                print("\nè il tuo ultimo tentativo.. fanne buon uso..")
+                continue
+
+            if tentativo1 >= 0:
+                print("\nhai " + str(tentativo1) + " tentativi prima della tua morte..")
+
+if risposta == "si" or risposta == "va bene" or risposta == "ok":
+    while tentativo1 >= 1:
+        print(
+            "\nVedere non si può e neanche sentire, fiutare non si può e neppure udire. Sta sotto i colli, sta dietro le stelle ed empie tutti i vuoti, tutte le celle. Per primo viene, ultimo va, a vita e a riso termine dà..")
+        risposta4 = input()
+        if risposta4 == "il buio" or risposta4 == "buio":
+            print("\nRisposta esatta!")
+            break
+        else:
+            tentativo1 -= 1
+            if tentativo1 == 0:
+                print("SEI MORTOOOOO!!!!!")
+                break
+
+            if tentativo1 == 1:
+                print("\nè il tuo ultimo tentativo.. fanne buon uso..")
+                continue
+
+            if tentativo1 >= 0:
+                print("\nhai " + str(tentativo1) + " tentativi prima della tua morte..")
+
+if risposta == "si" or risposta == "va bene" or risposta == "ok":
+    while tentativo1 >= 1:
+        print("\nVive senza respirare, freddo come morte pare, beve ma non è assetato, non tintinna corazzato.")
+        risposta5 = input()
+        if risposta5 == "il pesce" or risposta5 == "pesce":
+            print("\nRisposta esatta!")
+            break
+        else:
+            tentativo1 -= 1
+            if tentativo1 == 0:
+                print("SEI MORTOOOOO!!!!!")
+                break
+
+            if tentativo1 == 1:
+                print("\nè il tuo ultimo tentativo.. fanne buon uso..")
+                continue
+
+            if tentativo1 >= 0:
+                print("\nhai " + str(tentativo1) + " tentativi prima della tua morte..")
+
+if risposta == "si" or risposta == "va bene" or risposta == "ok":
+    while tentativo1 >= 1:
+        print(
+            "\nQuesta cosa ogni cosa divora, ciò che ha vita, la fauna, la flora; i re abbatte e così le città, rode il ferro, la calce già dura; e dei monti pianure farà.")
+        risposta6 = input()
+        if risposta6 == "il tempo" or risposta6 == "tempo":
+            print("\nRisposta esatta!")
+            break
+        else:
+            tentativo1 -= 1
+            if tentativo1 == 0:
+                print("SEI MORTOOOOO!!!!!")
+                break
+
+            if tentativo1 == 1:
+                print("\nè il tuo ultimo tentativo.. fanne buon uso..")
+                continue
+
+            if tentativo1 >= 0:
+                print("\nhai " + str(tentativo1) + " tentativi prima della tua morte..")
+
+if risposta == "si" or risposta == "va bene" or risposta == "ok":
+    while tentativo1 >= 1:
+        print("\nTrenta bianchi destrier su un colle rosso battono e mordono, ma nessun si è mosso.")
+        risposta7 = input()
+        if risposta7 == "i denti" or risposta7 == "denti":
+            print("\nRisposta esatta!")
+            break
+        else:
+            tentativo1 -= 1
+            if tentativo1 == 0:
+                print("SEI MORTOOOOO!!!!!")
+                break
+
+            if tentativo1 == 1:
+                print("\nè il tuo ultimo tentativo.. fanne buon uso..")
+                continue
+
+            if tentativo1 >= 0:
+                print("\nhai " + str(tentativo1) + " tentativi prima della tua morte..")
+
+if risposta == "si" or risposta == "va bene" or risposta == "ok":
+    while tentativo1 >= 1:
+        print(
+            "\nUn occhio in un azzurro viso vide un altro occhio dentro un verde viso: «Quell'occhio è come me, però è laggiù, mentre il mio occhio se ne sta quassù»")
+        risposta8 = input()
+        if risposta8 == "il sole e una margherita" or risposta8 == "sole e margherita":
+            print("\nRisposta esatta!")
+            break
+        else:
+            tentativo1 -= 1
+            if tentativo1 == 0:
+                print("SEI MORTOOOOO!!!!!")
+                break
+
+            if tentativo1 == 1:
+                print("\nè il tuo ultimo tentativo.. fanne buon uso..")
+                continue
+
+            if tentativo1 >= 0:
+                print("\nhai " + str(tentativo1) + " tentativi prima della tua morte..")
+
+if risposta == "si" or risposta == "va bene" or risposta == "ok":
+    while tentativo1 >= 1:
+        print("\nSenza coperchio, chiave, né cerniera uno scrigno cela una dorata sfera.")
+        risposta9 = input()
+        if risposta9 == "l'uovo" or risposta9 == "uovo":
+            print("\nRisposta esatta!")
+            break
+        else:
+            tentativo1 -= 1
+            if tentativo1 == 0:
+                print("SEI MORTOOOOO!!!!!")
+                break
+
+            if tentativo1 == 1:
+                print("\nè il tuo ultimo tentativo.. fanne buon uso..")
+                continue
+
+            if tentativo1 >= 0:
+                print("\nhai " + str(tentativo1) + " tentativi prima della tua morte..")
+
+if risposta == "si" or risposta == "va bene" or risposta == "ok":
+    while tentativo1 >= 1:
+        print(
+            "\nSenza-gambe sta su una gamba, Due-gambe vi siede accanto su Tre-gambe, Quattro-gambe ne prende un po'.")
+        risposta10 = input()
+        if risposta10 == "un pesce su un tavolinetto, un uomo a tavola seduto su uno sgabello, il gatto mangia le lische":
+            print("\nRisposta esatta!")
+            break
+        else:
+            tentativo1 -= 1
+            if tentativo1 == 0:
+                print("SEI MORTOOOOO!!!!!")
+                break
+
+            if tentativo1 == 1:
+                print("\nè il tuo ultimo tentativo.. fanne buon uso..")
+                continue
+
+            if tentativo1 >= 0:
+                print("\nhai " + str(tentativo1) + " tentativi prima della tua morte..")
+
+if risposta == "si" or risposta == "va bene" or risposta == "ok":
+    while tentativo1 >= 1:
+        print("\nChe cos'ho in tasca?")
+        risposta11 = input()
+        if risposta11 == "l'anello" or risposta11 == "anello":
+            print("\nRisposta esatta!")
+            print(
+                "COMPLIMENTI HAI COMPLETATO IL GIOCO WEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE SEI UNA BESTIA!!!!")
+            break
+        else:
+            tentativo1 -= 1
+            if tentativo1 == 0:
+                print("SEI MORTOOOOO!!!!!")
+                break
+
+            if tentativo1 == 1:
+                print("\nè il tuo ultimo tentativo.. fanne buon uso..")
+                continue
+
+            if tentativo1 >= 0:
+                print("\nhai " + str(tentativo1) + " tentativi prima della tua morte..")
